f1db9cb80ad5959ae4ec8ea313f44f0285e5ae3b
Upekka Chakma <upekkachakma@gmail.com>
2023-02-03 21:00:38 +0600

    changed ui

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,53 +1,42 @@
-import { MDBCol, MDBContainer, MDBRow } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBBtn } from 'mdbreact';
 import React from 'react';
 import personalPhoto from '../../Images/personal-photo.png';
-import NavbarPage from '../Navbar/NavbarPage';
 import Typewriter from 'typewriter-effect';
 
 const Home = () => {
-
+    // RiFacebookCircleLine, FiGithub SlSocialLinkedin
     return (
-        <div className="min-vh-100 d-flex justify-content-center align-items-center background position-relative">
-            <NavbarPage />
-            <MDBContainer fluid className="">
-                <MDBRow>
-                    <MDBCol md="6" sm="12" >
-                        <MDBRow>
-                            <MDBCol lg="12" className="d-flex justify-content-center align-items-center animate__animated animate__fadeInDown animate__delay-1s">
-                                <img style={{ minWidth: '18rem', maxWidth: '50%' }} src={personalPhoto} className="z-6 fancy-borderRadius" alt="" />
-                            </MDBCol>
-                        </MDBRow>
-                    </MDBCol>
-                    <MDBCol style={{ zIndex: 100 }} md="6" sm="12" className="d-flex flex-column justify-content-center mt-2" id="home">
-                        <h3 className="mb-4 font-500 text-white animate__animated animate__fadeInDown animate__delay-0.3s">
-                            Welcome I am
-                        </h3>
-                        <div>
-                            <h1 className="d-inline-block font-600 mb-4 gradient-color-text gradient-1 animate__animated animate__fadeInDown animate__delay-0.6s">
-                                Upekka Chakma
-                            </h1>
-                        </div>
-                        <h3 style={{ color: "#00ccff" }} className="font-400 mb-4 text-white animate__animated animate__fadeInUp animate__delay-2s">
-                            <Typewriter
-                                options={{
-                                    strings: ['I am a web developer'],
-                                    autoStart: true,
-                                    loop: true,
-                                }}
-                            />
-                        </h3>
-                        <p style={{ maxWidth: '400px' }} className="lh-lg mb-4 text-white animate__animated animate__fadeInUp animate__delay-1.5s">I am a junior web developer, currently dedicated myself to become even better in the web development field.</p>
-                        <a rel="noreferrer" href="https://drive.google.com/uc?export=download&id=15aNGJamac-mXchy7A5T-uCT_7I1z7E03">
-                            <button class="box animate__animated animate__fadeInUp animate__delay-1.5s">
-                                <span></span>
-                                <h3 className="font-400 text-center">My Resume</h3>
-                            </button>
-                        </a>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
+        <MDBContainer className="min-vh-100 d-flex flex-column justify-content-center align-items-center position-relative">
+            <h3 className="welcome gradient-color-text">
+                Welcome!
+            </h3>
+            <MDBRow className="d-flex justify-content-center align-items-center animate__animated animate__fadeInDown animate__delay-1s mb-3 bg-white-gradient-round">
+                <img style={{ width: 'clamp(6rem, 14vw, 14rem)', border: '2px solid transparent' }} src={personalPhoto} className="z-6 " alt="upekka" />
+            </MDBRow>
+            <MDBRow className="d-flex flex-column align-items-center mt-2" id="home">
 
-        </div>
+                <div>
+                    <h6 className="bg-white-gradient-2 text-white mb-4 animate__animated animate__fadeInDown animate__delay-0.6s">
+                        Upekka Chakma
+                    </h6>
+                </div>
+                <h6 style={{ color: "#00ccff" }} className="font-400 mb-4 text-white animate__animated animate__fadeInUp animate__delay-2s">
+                    <Typewriter
+                        options={{
+                            strings: ['Available for Work. Please Hire Me'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </h6>
+                <p style={{ maxWidth: '400px' }} className="lh-lg mb-4 text-white animate__animated animate__fadeInUp animate__delay-1.5s">I am a junior web developer, currently dedicated myself to become even better in the web development field.</p>
+                <a rel="noreferrer" href="https://drive.google.com/uc?export=download&id=15aNGJamac-mXchy7A5T-uCT_7I1z7E03">
+                    <MDBBtn color='primary' className="rounded-pill">
+                        my resume
+                    </MDBBtn>
+                </a>
+            </MDBRow>
+        </MDBContainer>
     );
 };
 
