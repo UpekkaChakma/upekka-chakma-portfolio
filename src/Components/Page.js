a2251bbde48b77359095260f66238570f851b0a6
Upekka Chakma <upekkachakma@gmail.com>
2023-02-04 17:24:26 +0600

    added css animations

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavbarPage from './Navbar/NavbarPage';
 import Particles from 'react-particles-js';
 import ConfigParticles from './Config/ConfigParticles';
 import MyRecord from './MyRecord/MyRecord';
@@ -15,18 +14,17 @@ const Page = () => {
 
     return (
         <div className="background px-2 px-md-5 position-relative">
-            <LeftSidebar />
-            <RightSidebar />
             <div className='position-absolute pe-none' style={{ inset: 0 }}>
                 <Particles height='300vh' width="100%" params={ConfigParticles} />
             </div>
-
-            <div className=''>
+            <LeftSidebar />
+            <RightSidebar />
+            <div>
                 <Home />
                 <MyRecord />
                 <Technologies />
                 <Service />
-                <ContactMe id="contact" />
+                <ContactMe />
             </div>
         </div>
     );
